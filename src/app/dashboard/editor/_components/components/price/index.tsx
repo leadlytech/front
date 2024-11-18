3d82a33bff2ca667e911e621ecec674aba0b6604
Luís Kobelnik <luis.feliperotondokobelnik@hotmail.com>
2024-11-18 04:36:13 -0300

    Price Component

--- src/app/dashboard/editor/_components/components/price/index.tsx
+++ src/app/dashboard/editor/_components/components/price/index.tsx
@@ -7,8 +7,7 @@ type Props = {
 export function PriceComponent({ component }: Props) {
   return (
     <div className="flex items-center border border-gray-300 rounded-lg p-4">
-      <div className="flex-grow">
-      </div>
+      <div className="flex-grow"></div>
       <div className="bg-gray-100 p-2 rounded-lg text-right">
         <p className="text-sm text-gray-500">10% off</p>
         <p className="text-2xl font-bold text-black">R$ 89,90</p>
